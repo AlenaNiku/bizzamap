26d55bdc624a23f0c9251b2fe3a60a156809caa5
Alena Nikulina <alenanikulina0@gmail.com>
2020-07-09 12:26:02 -0400

    added 3 locations

--- main.js
+++ main.js
@@ -10,3 +10,44 @@ let map = new mapboxgl.Map({
 
 // Add zoom and rotation controls to the map.
 map.addControl(new mapboxgl.NavigationControl());
+
+// custom geoJSON for rendering geospatial data of the business locations
+let geojson = {
+    type: 'FeatureCollection',
+    features: [{
+        type: 'Feature',
+        geometry: {
+            type: 'Point',
+            coordinates: [-118.253, 34.133]
+        },
+        properties: {
+            title: 'ServiceTitan, Glendale, CA',
+            description: 'ServiceTitan prides itself in providing an intuitive all-in-one software for home service businesses. However, the polished customer experience in their product was not reflected in the attendee experience of previous events. Prior to Bizzabo, ServiceTitan event attendees couldn’t see the event agenda on their phones or check-in onsite in an efficient manner. The registration tool was also separate from their other tools. As a result, it was difficult to sync and analyze data.',
+            img: 'images/Service-Titan.png'
+        }
+    },
+    {
+        type: 'Feature',
+        geometry: {
+            type: 'Point',
+            coordinates: [-122.265, 37.470]
+        },
+        properties: {
+            title: 'Gainsight, Redwood City, CA',
+            description: 'Gainsight is a SaaS platform that is largely responsible for defining the customer success category. Critical to Gainsight’s market presence is the Pulse brand.',
+            img: 'images/gainsight.png'
+        }
+    },
+    {
+        type: 'Feature',
+        geometry: {
+            type: 'Point',
+            coordinates: [-84.399, 33.779]
+        },
+        properties: {
+            title: 'GreenBook, Worldwide',
+            description: 'GreenBook is one of the foremost market research companies in the world. Every year they hold several global conferences to create connections within consumer insights by bringing together the most innovative voices in the industry. Out of all of these conferences none is greater than IIeX (Insight Innovation Exchange) North America, which features over 150 speakers for 3 full days of groundbreaking content.',
+            img: 'images/greenbook.png'
+        }
+    },]
+};

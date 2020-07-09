66185b9c371b7aad28a0d887a5e97a759a3d2454
Alena Nikulina <alenanikulina0@gmail.com>
2020-07-09 14:26:41 -0400

    added uber location

--- main.js
+++ main.js
@@ -210,6 +210,18 @@ let geojson = {
         type: 'Feature',
         geometry: {
             type: 'Point',
+            coordinates: [55.384, 25.162]
+        },
+        properties: {
+            title: 'Uber Technologies Inc, Dubai, UAE',
+            description: 'Uber Technologies, Inc., commonly known as Uber, is an American multinational ride-hailing company offering services that include peer-to-peer ridesharing, ride service hailing, food delivery, and a micromobility system with electric bikes and scooters.',
+            img: 'images/uber.jpeg'
+        }
+    },
+    {
+        type: 'Feature',
+        geometry: {
+            type: 'Point',
             coordinates: [-84.399, 33.779]
         },
         properties: {

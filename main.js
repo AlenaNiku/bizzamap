e73fc576a3e9a4e7235a2fba6e34855448083d92
Alena Nikulina <alenanikulina0@gmail.com>
2020-07-09 14:31:57 -0400

    added uber location

--- main.js
+++ main.js
@@ -246,6 +246,18 @@ let geojson = {
         type: 'Feature',
         geometry: {
             type: 'Point',
+            coordinates: [77.550, 12.873]
+        },
+        properties: {
+            title: 'Uber Technologies Inc, Bangalore, India',
+            description: 'Uber Technologies, Inc., commonly known as Uber, is an American multinational ride-hailing company offering services that include peer-to-peer ridesharing, ride service hailing, food delivery, and a micromobility system with electric bikes and scooters.',
+            img: 'images/uber.jpeg'
+        }
+    },
+    {
+        type: 'Feature',
+        geometry: {
+            type: 'Point',
             coordinates: [-84.399, 33.779]
         },
         properties: {

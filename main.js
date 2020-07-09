334c5fc0c99cfa99bed4038dd88ba69746e5047b
Alena Nikulina <alenanikulina0@gmail.com>
2020-07-09 12:28:42 -0400

    making a marker for each feature and adding it to the map

--- main.js
+++ main.js
@@ -59,5 +59,10 @@ geojson.features.forEach(marker => {
     let el = document.createElement('div');
     el.className = 'marker';
 
-    
+    // make a marker for each feature and add to the map
+    new mapboxgl.Marker(el)
+        .setLngLat(marker.geometry.coordinates)
+        .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+        .setHTML(`<h3>${marker.properties.title}</h3><img src="${marker.properties.img}"</img><p>${marker.properties.description}</p>`))
+        .addTo(map);
 });

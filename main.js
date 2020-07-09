8e5eac0d90421111ce7b8cfc35f63bf50ba7c8de
Alena Nikulina <alenanikulina0@gmail.com>
2020-07-09 14:45:51 -0400

    changed starting position and zoom

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiYWxpbmlrdWwiLCJhIjoiY2tiY28xbHd4MDNyMzJybnR0c
 let map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/alinikul/ckceg8r0r0wps1iphme3xc375', // my custom stylesheet location
-    center: [-17.32, 27.37], // starting position [lng, lat]
-    zoom: 1.5 // starting zoom
+    center: [-0.32, 27.37], // starting position [lng, lat]
+    zoom: 1.7 // starting zoom
 });
 
 // Add zoom and rotation controls to the map.

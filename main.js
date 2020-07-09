b42dd13fa686b57b0a17e27bc6de4e33199f7b4b
Alena Nikulina <alenanikulina0@gmail.com>
2020-07-09 11:44:59 -0400

    added the accsess token and initializing the map with custom stylesheet

---
+++ main.js
@@ -0,0 +1,9 @@
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWxpbmlrdWwiLCJhIjoiY2tiY28xbHd4MDNyMzJybnR0czZqYW1xbSJ9.z_-zeSM0wGBIMbwTVT9J-A';
+
+let map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/alinikul/ckceg8r0r0wps1iphme3xc375', // my custom stylesheet location
+    center: [-96, 37.8], // starting position [lng, lat]
+    zoom: 3.5 // starting zoom
+});

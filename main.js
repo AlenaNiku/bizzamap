4a6bfeb61da75066a303498183f0a9561f5ca2c7
Alena Nikulina <alenanikulina0@gmail.com>
2020-07-09 14:10:23 -0400

    added santiago uber location

--- main.js
+++ main.js
@@ -138,6 +138,18 @@ let geojson = {
         type: 'Feature',
         geometry: {
             type: 'Point',
+            coordinates: [-70.646, -33.441]
+        },
+        properties: {
+            title: 'Uber Technologies Inc, Santiago, Chile',
+            description: 'Uber Technologies, Inc., commonly known as Uber, is an American multinational ride-hailing company offering services that include peer-to-peer ridesharing, ride service hailing, food delivery, and a micromobility system with electric bikes and scooters.',
+            img: 'images/uber.jpeg'
+        }
+    },
+    {
+        type: 'Feature',
+        geometry: {
+            type: 'Point',
             coordinates: [-84.399, 33.779]
         },
         properties: {

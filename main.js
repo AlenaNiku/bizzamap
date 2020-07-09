591ed16a5b0f3db2f3856e61e9da888777b93bb3
Alena Nikulina <alenanikulina0@gmail.com>
2020-07-09 14:05:32 -0400

    added new uber locations

--- main.js
+++ main.js
@@ -114,6 +114,18 @@ let geojson = {
         type: 'Feature',
         geometry: {
             type: 'Point',
+            coordinates: [-79.396, 43.676]
+        },
+        properties: {
+            title: 'Uber Technologies Inc, Toronto, Canada',
+            description: 'Uber Technologies, Inc., commonly known as Uber, is an American multinational ride-hailing company offering services that include peer-to-peer ridesharing, ride service hailing, food delivery, and a micromobility system with electric bikes and scooters.',
+            img: 'images/uber.jpeg'
+        }
+    },
+    {
+        type: 'Feature',
+        geometry: {
+            type: 'Point',
             coordinates: [-84.399, 33.779]
         },
         properties: {

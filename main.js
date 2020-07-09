7c1a6f68185b84ea3d1bcba5b603eabc50ae75ae
Alena Nikulina <alenanikulina0@gmail.com>
2020-07-09 14:08:02 -0400

    added new uber locations

--- main.js
+++ main.js
@@ -126,6 +126,18 @@ let geojson = {
         type: 'Feature',
         geometry: {
             type: 'Point',
+            coordinates: [-46.639, -23.551]
+        },
+        properties: {
+            title: 'Uber Technologies Inc, Sao Paulo, Brazil',
+            description: 'Uber Technologies, Inc., commonly known as Uber, is an American multinational ride-hailing company offering services that include peer-to-peer ridesharing, ride service hailing, food delivery, and a micromobility system with electric bikes and scooters.',
+            img: 'images/uber.jpeg'
+        }
+    },
+    {
+        type: 'Feature',
+        geometry: {
+            type: 'Point',
             coordinates: [-84.399, 33.779]
         },
         properties: {
